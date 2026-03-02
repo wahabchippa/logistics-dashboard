74ded28c10bb096889ef7c6d491cea95f0da593a
wahabchippa <wahab.chippa@joinfleek.com>
2026-03-02 16:25:48 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4345,3 +4345,6 @@ def nexus_dashboard():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
