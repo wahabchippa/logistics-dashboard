c9a33b18d845044fceeb022afa41f45010dff822
wahabchippa <wahab.chippa@joinfleek.com>
2026-03-06 03:19:07 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5692,6 +5692,5 @@ def home():
     return "<html><body><h1 style='font-family:sans-serif'>Home</h1><p>Go to /bundling</p></body></html>"
 
 
-
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
