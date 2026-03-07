e3418f975adf052b2ded0eab9a90830059b80058
wahabchippa <wahab.chippa@joinfleek.com>
2026-03-08 00:38:44 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1276,6 +1276,7 @@ USERS = {
     "albash@joinfleek.com":       "Albash123",
     "waris@joinfleek.com":        "waris123",
     "moiz@joinfleek.com":         "moiz1234",
+    "areeb@joinfleek.com"         "areeb1234"
 }
 
 # Guest login ke liye ek simple password (optional, agar seedha guest button chahiye)
